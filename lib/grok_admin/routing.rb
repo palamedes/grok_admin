ef5e584ad5d9ea4cb72b9dcb69410d6dbf2686d8
Jason Ellis <palamedes@rocketmail.com>
2015-03-19 14:18:45 -0400

    WOops it's a class

--- lib/grok_admin/routing.rb
+++ lib/grok_admin/routing.rb
@@ -1,9 +1,11 @@
+# Add a class to the routing mapper to allow us to custom set the route name
 module ActionDispatch
   module Routing
     class Mapper
 
       # Allow a user to set the route path to anything
       def grokadmin_to route_name
+        # todo Make sure the route name is a legal one?
         mount GrokAdmin::Engine => route_name
       end
 
