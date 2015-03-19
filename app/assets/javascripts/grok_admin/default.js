87c542c04258ea956c1d5526287cba8843a7534e
Jason Ellis <palamedes@rocketmail.com>
2015-03-19 18:54:39 -0400

    When you click edit, it now knows how to swap to an input

--- app/assets/javascripts/grok_admin/default.js
+++ app/assets/javascripts/grok_admin/default.js
@@ -1,26 +1,37 @@
 // Once turbo links has fired, then do stuff.
 $(document).on('ready page:load', function () {
-    // Turn on all dropdowns
-    $('.ui.dropdown').dropdown();
-    // Turn on all radio buttons
-    $('.ui.checkbox').checkbox();
-    // Turn on all accordions
-    $('.ui.accordion').accordion();
-    // Turn on all popups
-    $('.popup').popup();
-    // Tabs
-    $('.tabular.menu .item').tab();
-    // Date pickers
-    $('.datepicker').datepicker({"dateFormat": "DD, d MM, yy"});
+  // Turn on all dropdowns
+  $('.ui.dropdown').dropdown();
+  // Turn on all radio buttons
+  $('.ui.checkbox').checkbox();
+  // Turn on all accordions
+  $('.ui.accordion').accordion();
+  // Turn on all popups
+  $('.popup').popup();
+  // Tabs
+  $('.tabular.menu .item').tab();
+  // Date pickers
+  $('.datepicker').datepicker({"dateFormat": "DD, d MM, yy"});
 
 
-    // When the user clicks the edit icon for a row do things!
-    $(document).on('click', 'i.edit.icon', function(e) {
-        $columns = $(this).parent().siblings();
-
-        $columns.each(function(index, td) {
-            console.log($(td).text());
-        });
+  // When the user clicks the edit icon for a row do things!
+  $(document).on('click', 'i.edit.icon', function(e) {
+    // Get the columns for this edit icon based on its siblings (so we dont get it)
+    $columns = $(this).parent().siblings();
+    $columns.each(function(index, td) {
+      // Get our col size so we know how big to make the input
+      var colSize = $(td).outerWidth();
+      // Get our column data
+      var value = $(td).text();
+      // Store the original data just incase
+      $(td).data('original-value', value);
+      // Build our input
+      // todo build it based on the type of input in data-type
+      var html = "<input type='text' value='" + value + "' />"
+      // Inject the input and focus it.
+      $(td).html(html);
+      $(td).find('input').css('width', colSize - 20);
     });
+  });
 
 });
