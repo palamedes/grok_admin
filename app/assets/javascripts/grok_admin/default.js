428717b5b9cbaefbd1bed66572a4f791940e2cc1
Jason Ellis <palamedes@rocketmail.com>
2015-03-19 19:03:01 -0400

    Wrap our input in mini tags

--- app/assets/javascripts/grok_admin/default.js
+++ app/assets/javascripts/grok_admin/default.js
@@ -38,7 +38,7 @@ $(document).on('ready page:load', function () {
       }
       // Build our input
       // todo build it based on the type of input in data-type
-      var html = "<input type='" + type + "' value='" + value + "' />"
+      var html = "<div class='ui mini input'><input type='" + type + "' value='" + value + "' /></div>";
       // Inject the input and focus it.
       $(td).html(html);
       $(td).find('input').css('width', colSize - 20);
