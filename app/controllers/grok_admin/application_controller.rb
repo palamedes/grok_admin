d892829bc4b9e8418d1e8b70c1fbe820bfbf3d6a
Jason Ellis <palamedes@rocketmail.com>
2015-03-19 14:52:29 -0400

    Should correctly dump all models in the parent application

--- app/controllers/grok_admin/application_controller.rb
+++ app/controllers/grok_admin/application_controller.rb
@@ -1,6 +1,12 @@
 module GrokAdmin
   class ApplicationController < ActionController::Base
+
+    # Default dashboard view
     def index
+      # Get all of the model classes in the system
+      @models = ActiveRecord::Base.connection.tables.collect{|t| t.classify.constantize rescue nil }.compact
+
     end
+
   end
 end
