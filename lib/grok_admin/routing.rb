02c5f996288c34564b88f0231e82a36775af3aea
Jason Ellis <palamedes@rocketmail.com>
2015-03-19 14:09:42 -0400

    Working on routing map

---
+++ lib/grok_admin/routing.rb
@@ -0,0 +1,8 @@
+module ActionDispatch::Routing::Mapper
+
+  # Allow a user to set the route path to anything
+  def grokadmin_to route_name
+    mount GrokAdmin::Engine => route_name
+  end
+
+end
--- lib/grok_admin/version.rb
+++ lib/grok_admin/version.rb
@@ -1,3 +1,3 @@
 module GrokAdmin
-  VERSION = "0.0.8"
+  VERSION = "0.0.10"
 end
