aae207419ddebf4623eed07ff4eaa380ec4581d0
Jason Ellis <palamedes@rocketmail.com>
2015-03-20 10:17:55 -0400

    Added method to return the model in the association

--- app/helpers/grok_admin/application_helper.rb
+++ app/helpers/grok_admin/application_helper.rb
@@ -7,6 +7,15 @@ module GrokAdmin
       model.reflect_on_all_associations.select{|assoc| assoc.macro == :belongs_to }.map(&:foreign_key).include?(column)
     end
 
+    # Reflect on all the associations and tell me the actual model of the column I'm sending as an association
+    def getRelationModel model, column
+      # Get associations from the model, Only those that are :belongs_to, and map those to key, model
+      associations = Assignment.reflect_on_all_associations
+                               .select{|assoc| assoc.macro == :belongs_to }
+                               .map{|assoc| [ assoc.foreign_key, assoc.plural_name.singularize.classify.constantize ] }
+      # now return the model
+      Hash[*associations.flatten][column]
+    end
 
   end
 end
