5362afd8df2206e291c828a7ca77197114c4099b
Jason Ellis <palamedes@rocketmail.com>
2015-03-22 14:05:05 -0400

    JS update around getting the next page for a given model.  ~A: Spoiled Milk!

--- app/assets/javascripts/grok_admin/pagination.js
+++ app/assets/javascripts/grok_admin/pagination.js
@@ -3,7 +3,26 @@ $(document).on('ready page:load', function () {
 
   // On click of a pagination item
   $(document).on('click', '.ui.pagination.menu a.item:not(.disabled):not(.active)', function() {
-    console.log(this);
+
+    var route = $(this).data('route');
+    var model = $(this).data('model');
+    var page = $(this).data('page');
+    var data = "model=" + model + "&page=" + page;
+
+    // Create an ajax put to the route to get the next batch of records for our paginator
+    $.ajax({
+      type: "GET",
+      dataType: "JSON",
+      url: route,
+      data: data,
+      success: function(resp) {
+        console.log(arguments);
+      },
+      error: function() {
+        console.error(arguments);
+      }
+    });
+
   });
 
 });
