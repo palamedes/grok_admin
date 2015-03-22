77a2d67e9063f215f8f3d4ce7111161bd9079069
Jason Ellis <palamedes@rocketmail.com>
2015-03-22 13:38:05 -0400

    Adding test for clicking non-disabled pagination menu items  ~Q: Whats the difference between a guitar and a fish?

--- app/assets/javascripts/grok_admin/pagination.js
+++ app/assets/javascripts/grok_admin/pagination.js
@@ -1,4 +1,9 @@
 // Once turbo links has fired, then do stuff.
 $(document).on('ready page:load', function () {
 
+  // On click of a pagination item
+  $(document).on('click', '.ui.pagination.menu a.item:not(disabled)', function() {
+    console.log(this);
+  });
+
 });
