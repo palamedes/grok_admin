5e4c504d7c416415b50a82d5201df04fce894cd5
Jason Ellis <palamedes@rocketmail.com>
2015-03-22 13:42:41 -0400

    Hrmm maybe that selector will work better.  ~Q:  Did you hear the one about the hungry clock?

--- app/assets/javascripts/grok_admin/pagination.js
+++ app/assets/javascripts/grok_admin/pagination.js
@@ -2,7 +2,7 @@
 $(document).on('ready page:load', function () {
 
   // On click of a pagination item
-  $(document).on('click', '.ui.pagination.menu a.item:not(.active .disabled)', function() {
+  $(document).on('click', '.ui.pagination.menu a.item:not(.disabled):not(.active)', function() {
     console.log(this);
   });
 
