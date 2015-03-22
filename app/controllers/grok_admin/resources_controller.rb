781880ac63d6a0378af10391f85dcb93e763924e
Jason Ellis <palamedes@rocketmail.com>
2015-03-22 18:51:07 -0400

    Refactor of the resources controller to add some before filters to do the smart things..

--- app/controllers/grok_admin/resources_controller.rb
+++ app/controllers/grok_admin/resources_controller.rb
@@ -1,15 +1,12 @@
 module GrokAdmin
   class ResourcesController < ApplicationController
 
+    before_action :set_model, only: [:index, :update, :destroy]
+    before_action :set_resource, only: [:update, :destroy]
+
     # Default dashboard view
+    # GET /resources&model={model}&page={page}
     def index
-      # Get the resource we are asking for from the params ~ If there is no resource, redirect to root
-      resource = params['model']
-      # Convert it to a model, or nil (Rescue isn't the best way to do this but it works)
-      @model = resource.classify.constantize rescue nil
-
-      # todo if @model is nil, get us out of here
-
       # Get the "page" from the params too, if there is no page give them page zero
       page = params['page'] || 0
       offset = page.to_i * GrokAdmin::PAGINATION_SIZE
@@ -23,6 +20,37 @@ module GrokAdmin
           model: resource,
           showing: "Showing #{offset + 1} - #{offset + @records.count} of #{@model.all.count}"
       }
+    end
+
+    # PATCH/PUT /projects/1
+    def update
+      if @resource.update(resource_params)
+        format.json { head :no_content }
+      else
+        format.json { render json: @resource.errors, status: :unprocessable_entity }
+      end
+    end
+
+    private
+
+    # Before filter to set the model we are talking about
+    def set_model
+      # Get the resource we are asking for from the params ~ If there is no resource, redirect to root
+      resource = params['model']
+      # Convert it to a model, or nil (Rescue isn't the best way to do this but it works)
+      @model = resource.classify.constantize rescue nil
+
+      # todo if @model is nil, get us out of here
+    end
+
+    # Load the resource we are talking about here
+    def set_resource
+      @resource = @model.find by: params[:id]
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def resource_params
+      # todo dynamically set the allowed params based on what we are allowed to set
     end
 
   end
