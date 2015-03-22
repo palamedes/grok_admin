79f554db94a09f15dbf15ced707015c9ab660d06
Jason Ellis <palamedes@rocketmail.com>
2015-03-22 16:22:41 -0400

    We need the column as the input name

--- app/assets/javascripts/grok_admin/editor.js
+++ app/assets/javascripts/grok_admin/editor.js
@@ -33,7 +33,8 @@ $(document).on('ready page:load', function () {
       }
       // Build our input
       // todo build it based on the type of input in data-type
-      var html = "<input class='editor' type='" + type + "' value='" + value + "' />";
+      var column = $(td).data('column');
+      var html = "<input class='editor' type='" + type + "' name='resource[' + column + ']' value='" + value + "' />";
       // Inject the input and focus it.
       $(td).html(html);
       $(td).find('input').css('width', colSize - 20);
